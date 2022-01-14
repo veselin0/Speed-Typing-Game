eebdeaf995668102209a6e21ad9a245f8952afe5
Veselin <vesselin60@gmail.com>
2022-01-14 09:02:52 +0200

    add start game function

--- src/App.js
+++ src/App.js
@@ -3,14 +3,15 @@ import { useEffect, useState } from "react";
 const App = () => {
     const [text, setText] = useState("");
     const [countdown, setCoundown] = useState(5);
+    const [isGameRunning, setIsGameRunning] = useState(false);
 
     useEffect(() => {
-        if (countdown > 0) {
+        if (countdown > 0 && isGameRunning) {
             setTimeout(() => {
                 setCoundown((prevCountdown) => prevCountdown - 1);
             }, 1000);
         }
-    }, [countdown]);
+    }, [countdown, isGameRunning]);
 
     const handleTextChange = (event) => {
         const { value } = event.target;
@@ -25,6 +26,11 @@ const App = () => {
         console.log(count);
     };
 
+    const startGame = () => {
+        setIsGameRunning(isGameRunning => !isGameRunning);
+        wordCount(text);
+    }
+
     return (
         <div>
             <h1>How fast do you type?</h1>
@@ -34,7 +40,7 @@ const App = () => {
                 onChange={handleTextChange}
             />
             <h4>Time Remaining: {countdown}s</h4>
-            <button onClick={() => wordCount(text)}>Start</button>
+            <button onClick={() => startGame()}>Start</button>
             <h1>Word Count: </h1>
         </div>
     );
