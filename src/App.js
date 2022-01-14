e4b1b9b0ac381cc218189032ef950092ee279ec4
Veselin <vesselin60@gmail.com>
2022-01-14 08:38:29 +0200

    add countdown timer

--- src/App.js
+++ src/App.js
@@ -1,37 +1,43 @@
-import {useState} from 'react';
+import { useEffect, useState } from "react";
 
 const App = () => {
-    const [text, setText] = useState("")
+    const [text, setText] = useState("");
+    const [countdown, setCoundown] = useState(5);
+
+    useEffect(() => {
+        if (countdown > 0) {
+            setTimeout(() => {
+                setCoundown((prevCountdown) => prevCountdown - 1);
+            }, 1000);
+        }
+    }, [countdown]);
 
     const handleTextChange = (event) => {
-        const {value} = event.target;
+        const { value } = event.target;
         setText(value);
-    }
-    
+    };
+
     const wordCount = (text) => {
         const count = text
             .trim()
-            .split(' ')
-            .filter(word => word !== "")
-            .length;
+            .split(" ")
+            .filter((word) => word !== "").length;
         console.log(count);
-    }
-
-    
+    };
 
     return (
         <div>
             <h1>How fast do you type?</h1>
-            <textarea 
-                placeholder="Type here!" 
+            <textarea
+                placeholder="Type here!"
                 value={text}
                 onChange={handleTextChange}
             />
-            <h4>Time Remaining: </h4>
+            <h4>Time Remaining: {countdown}s</h4>
             <button onClick={() => wordCount(text)}>Start</button>
             <h1>Word Count: </h1>
         </div>
-    ); 
-}
+    );
+};
 
 export default App;
